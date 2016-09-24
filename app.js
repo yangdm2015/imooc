9d542ab0ce18acd20b7d8e31f6650e9b2f87d76a
yangdm2015 <yangshan503@126.com>
2016-09-24 11:40:39 +0800

    test Signed-off-by: yangdm2015 <yangshan503@126.com>

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views','./views/jade')
 app.set('view engine','jade')
 /*app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())*/
-
+ 
 app.use(bodyParser.json({limit: '1mb'}));  //body-parser 解析json格式数据
 app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
   extended: true
